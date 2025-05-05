f32b024d1822d71ad917a7b84e522c57f7e9506a
karasu354 <karasuyama1997@gmail.com>
2025-05-06 05:42:01 +0900

    feature:edit player ui

--- components/PlayerEditCard.tsx
+++ components/PlayerEditCard.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import { Player } from '../utils/player'
+import { isNoRankTier, rankEnum, tierEnum } from '../utils/rank'
+import { roleEnum } from '../utils/role'
+import CheckBox from './Input/CheckBox'
+import Dropdown from './Input/Dropdown'
+import InputText from './Input/InputText'
 
 interface PlayerEditCardProps {
   currentPlayer: Player
@@ -13,36 +18,142 @@ const PlayerEditCard: React.FC<PlayerEditCardProps> = ({
   setEditablePlayer,
   onEditModeToggle,
 }) => {
-  const [name, setName] = React.useState(currentPlayer.name)
+  const [player, setPlayer] = React.useState<Player>(
+    Player.fromJson(currentPlayer.playerInfo)
+  )
 
-  const handleUpdatePlayer = (e: React.MouseEvent) => {
+  const updatePlayerProperty = <K extends keyof Player>(
+    key: K,
+    value: Player[K]
+  ) => {
+    setPlayer((prevPlayer) => {
+      const updatedPlayer = Player.fromJson(prevPlayer.playerInfo)
+      Object.assign(updatedPlayer, prevPlayer, { [key]: value })
+      if (key === 'tier' || key === 'rank') {
+        updatedPlayer.setCalculatedRating()
+        updatedPlayer.setDisplayRank()
+      }
+      return updatedPlayer
+    })
+  }
+
+  const handleSetDesiredRoles = (roles: roleEnum[]) => {
+    if (roles.includes(roleEnum.all)) {
+      updatePlayerProperty(
+        'desiredRoles',
+        Object.values(roleEnum).filter((role) => role !== roleEnum.all)
+      )
+    } else {
+      updatePlayerProperty('desiredRoles', roles)
+    }
+  }
+
+  const handleSave = (e: React.MouseEvent) => {
     e.stopPropagation()
-    currentPlayer.name = name
+    setEditablePlayer(player)
     onEditModeToggle(e)
   }
 
   return (
-    <div className="mt-4 border-t pt-4">
-      <div className="space-y-4">
-        <div>
-          <input
-            type="text"
-            placeholder="Player Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full rounded border border-gray-300 p-2"
+    <div className="p-2">
+      <div className="space-y-2">
+        <p className="font-bold">Player Edit</p>
+        <InputText
+          value={player.name}
+          setValue={(value) => updatePlayerProperty('name', value)}
+          label="Player Name"
+        />
+        <Dropdown
+          value={player.tier}
+          setValue={(value) => updatePlayerProperty('tier', value as tierEnum)}
+          label="Tier"
+          options={Object.values(tierEnum).map((tierValue) => ({
+            label: tierValue,
+            value: tierValue,
+          }))}
+        />
+        {isNoRankTier(player.tier) === false && (
+          <div className="flex items-center space-x-2">
+            <p>┗</p>
+            <Dropdown
+              value={player.rank}
+              setValue={(value) =>
+                updatePlayerProperty('rank', value as rankEnum)
+              }
+              label="Rank"
+              options={Object.values(rankEnum).map((rankValue) => ({
+                label: rankValue,
+                value: rankValue,
+              }))}
+            />
+          </div>
+        )}
+
+        <Dropdown
+          value={player.mainRole}
+          setValue={(value) =>
+            updatePlayerProperty('mainRole', value as roleEnum)
+          }
+          label="Main Role"
+          options={Object.values(roleEnum).map((roleValue) => ({
+            label: roleValue,
+            value: roleValue,
+          }))}
+        />
+        {player.mainRole !== roleEnum.all && (
+          <Dropdown
+            value={player.subRole}
+            setValue={(value) =>
+              updatePlayerProperty('subRole', value as roleEnum)
+            }
+            label="Sub Role"
+            options={Object.values(roleEnum)
+              .filter(
+                (role) => role !== roleEnum.all && role !== player.mainRole
+              )
+              .map((roleValue) => ({
+                label: roleValue,
+                value: roleValue,
+              }))}
           />
+        )}
+
+        <CheckBox
+          values={player.desiredRoles}
+          setValues={(roles) => handleSetDesiredRoles(roles as roleEnum[])}
+          label="Desired Roles"
+          options={Object.values(roleEnum).map((roleValue) => ({
+            label: roleValue,
+            value: roleValue,
+          }))}
+        />
+
+        {/* Role Fixed */}
+        <div className="flex items-center space-x-2">
+          <input
+            type="checkbox"
+            checked={player.isRoleFixed}
+            onChange={(e) =>
+              updatePlayerProperty('isRoleFixed', e.target.checked)
+            }
+            id="role-fixed-toggle"
+          />
+          <label htmlFor="role-fixed-toggle" className="text-sm">
+            Role Fixed
+          </label>
         </div>
+
+        {/* Buttons */}
         <div className="flex justify-end space-x-2">
           <button
-            onClick={handleUpdatePlayer}
+            onClick={handleSave}
             className="rounded bg-green-500 px-4 py-2 text-white transition hover:bg-green-600"
           >
             保存
           </button>
           <button
             onClick={onEditModeToggle}
-            className="rounded bg-gray-500 px-4 py-2 text-white transition hover:bg-gray-600"
+            className="rounded bg-red-500 px-4 py-2 text-white transition hover:bg-gray-600"
           >
             キャンセル
           </button>
