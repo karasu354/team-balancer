4416d9907f3e7730aee7e7e6e92659ad4d7c25b4
Shumpei Karasuyama <karasuyama1997@gmail.com>
2025-05-06 21:18:29 +0900

    fix: change header design (#67)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,18 @@ import './globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header className="fixed top-0 left-0 z-50 h-20 w-full bg-white shadow-md">
-        <nav className="mx-auto flex w-full max-w-6xl items-center justify-between p-4 text-gray-800">
-          <div className="flex items-center space-x-2">
-            <img src="/logo.svg" alt="Team Balancer ロゴ" className="h-8 w-8" />
-            <div className="text-2xl font-bold">Team Balancer</div>
+      <header className="w-full border-b-2 border-gray-200 bg-white">
+        <nav className="mx-auto flex w-full max-w-6xl items-center justify-between p-4">
+          <div className="flex items-center space-x-4">
+            <img src="/logo.svg" alt="Team Balancer Logo" className="h-8 w-8" />
+            <div className="text-2xl font-bold text-blue-500">
+              TEAM BALANCER
+            </div>
           </div>
         </nav>
       </header>
 
-      <div className="pt-20">
+      <div className="">
         <Component {...pageProps} />
       </div>
     </>
